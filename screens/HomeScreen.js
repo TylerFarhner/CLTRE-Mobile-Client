d3f1d5daea73950bda5dc034ae814d1763456c89
Tyler Farhner <tylerfarhner@gmail.com>
2021-11-30 22:00:29 -0800

    retreives tokens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function HomeScreen() {
+export default function HomeScreen(props) {
+
+    const loadProfile = async () => {
+        const token = await AsyncStorage.getItem('token')
+        if(!token) {
+            props.navigation.navigate('Login')
+        }
+        console.log(token)
+    }
+
+    useEffect(() => {
+        loadProfile()
+    })
 
     return(
         <View>
